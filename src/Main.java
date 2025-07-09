f5a8fda78e78f87cbeb8eff2b6f71d2b7f9e2be1
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-07-09 15:04:26 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String msg = "Programando em ";
+        System.out.println("Hello World.");
+        System.out.println(msg + args[0]);
+        System.out.println("Precisamos de uma " + args[1]);
+        }
+}
