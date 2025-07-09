1795a0e27fb0de431f7c15c81338b3f213f579e7
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-07-09 15:11:03 -0300

    feat: Inicio

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,5 +8,6 @@ public class Main {
         System.out.println("Hello World.");
         System.out.println(msg + args[0]);
         System.out.println("Precisamos de uma " + args[1]);
+        System.out.println("Sucesso!");
         }
 }
