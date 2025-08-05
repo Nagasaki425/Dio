6a89c8165312eea4f796a8459a3cbe77a3a77997
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-08-05 18:40:54 -0300

    Aula 2 feita e funcionando

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-     
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Quanto é 2 + 2 ? ");
+        var result = scanner.nextInt();
+        var isRight = (result == 4) ? "sim" : "nao";
+
+
+        System.out.println("O resultado é 4, você acertou? " + isRight);
+
+        scanner.close();
         }
 }
