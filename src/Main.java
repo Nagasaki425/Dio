339e27d28db15d5609476d4ff7d4f887d3d87630
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-08-05 18:38:57 -0300

    Fix: template para as aulas

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String msg = "Programando em ";
-        System.out.println("Hello World.");
-        System.out.println(msg + args[0]);
-        System.out.println("Precisamos de uma " + args[1]);
+     
         }
 }
