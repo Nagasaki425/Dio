194ce82729b1ad9ef94844bee6af811915c50e77
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-08-05 18:36:31 -0300

    aula 1 feito

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,24 @@ import java.util.Scanner;
 
 
     public static void main(String[] args){
-        var scanner = new Scanner(System.in);
+        
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(WELCOME_MESSAGE);
         var name = scanner.next();
+        
         System.out.println("Informe a sua idade: ");
         var age = scanner.nextInt();
         System.out.println("Olá " +name+ " sua idade é " +age+ " anos." );
+
+        System.out.println("Quanto é 2 + 2 ? ");
+        var result = scanner.nextInt();
+        var isRight = (result == 4) ? "sim" : "nao";
+
+
+        System.out.println("O resultado é 4, você acertou? " + isRight);
+
+        scanner.close();
+
     }
 }
