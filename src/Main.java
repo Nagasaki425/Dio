59b648c592d6f9eb4465dedbcf4ed0277eb7a220
Victor Gargalhone <victorgargalhone04@gmail.com>
2025-08-11 19:00:36 -0300

    Aula 3 feita

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-     
+
+        /*
+         * Adicionar
+         * comentários
+         * de varias
+         * linhas
+         */
+        // Adicionar comentário de uma linha
+
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Quantos anos você tem ?");
+        var age = scanner.nextInt();
+
+        if (age < 18) {
+            System.out.println("Você é emancipado ?");
+            var resposta = scanner.next().trim().toLowerCase();
+
+            if (resposta.equals("sim") && age >= 16) {
+                System.out.println("Você pode dirigir !");
+
+            } else {
+                System.out.println("Você não pode dirigir !");
+            }
+
         }
+
+        else {
+            System.out.println("Você pode dirigir !");
+        }
+
+    }
 }
